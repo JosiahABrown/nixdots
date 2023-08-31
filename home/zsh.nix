11562ec759277e8f82c9b011b4fbf2bf0eb7fc0a
Josiah Brown <josiah.alen.brown@gmail.com>
2023-08-31 16:27:51 -0500

    Added more aliases to zshrc

--- home/zsh.nix
+++ home/zsh.nix
@@ -6,15 +6,25 @@
     enableSyntaxHighlighting = true;
     shellAliases = {
       la = "ls -a";
+      lla = "ls -al";
       update = "sudo nixos-rebuild switch";
+      nix-config = "cd /etc/nixos && sudo -E nvim configuration.nix";
       v = "nvim";
       _ = "sudo";
+      ga = "git add";
+      gaa = "git add -A";
+      gcm = "git commit -m";
+      up = "cd ..";  
+      webcam = "gphoto2 --stdout --capture-movie | ffmpeg -i - -vcodec rawvideo -pix_fmt yuv420p -threads 0 -f v4l2 /dev/video0";
+      h = "history";
+      hg = "history | grep";
     };
     oh-my-zsh = {
       enable = true;
       plugins = [ 
         "docker-compose" 
         "docker" 
+        "git"
       ];
       theme = "refined";
     };
