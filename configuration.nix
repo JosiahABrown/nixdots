bc5a76eea479a90f1985266cfe10895dfc97faf0
Josiah Brown <josiah.alen.brown@gmail.com>
2023-08-31 15:01:55 -0500

    Added rest of configuration.nix -> base.nix

--- configuration.nix
+++ configuration.nix
@@ -10,119 +10,9 @@
       /etc/nixos/hardware-configuration.nix
       /etc/nixos/system/networking.nix
       /etc/nixos/system/packages.nix
-      <home-manager/nixos>
+      /etc/nixos/system/base.nix
     ];
  
-  # ENV variables
-  environment.variables = {
-    EDITOR = "nvim";
-  };
- 
-  # Bootloader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-  boot.extraModulePackages = [ config.boot.kernelPackages.v4l2loopback.out ];
-  boot.kernelModules = [
-    "v4l2loopback"
-  ];
-  boot.extraModprobeConfig = ''
-    options v4l2loopback exclusive_caps=1 card_label="Virtual Camera"
-  '';
-
-  # Set your time zone.
-  time.timeZone = "America/Chicago";
-
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
-
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "en_US.UTF-8";
-    LC_IDENTIFICATION = "en_US.UTF-8";
-    LC_MEASUREMENT = "en_US.UTF-8";
-    LC_MONETARY = "en_US.UTF-8";
-    LC_NAME = "en_US.UTF-8";
-    LC_NUMERIC = "en_US.UTF-8";
-    LC_PAPER = "en_US.UTF-8";
-    LC_TELEPHONE = "en_US.UTF-8";
-    LC_TIME = "en_US.UTF-8";
-  };
-
-  # Configure keymap in X11
-  services.xserver = {
-    layout = "us";
-    xkbVariant = "";
-    enable = true;
-    displayManager.sddm.enable = true;
-  };
- 
-  # Home manager
-  home-manager = {
-    useGlobalPkgs = true;
-    useUserPackages = true;
-    users.josiah = import ./home.nix;
-  };
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.josiah = {
-    isNormalUser = true;
-    description = "Josiah";
-    shell = pkgs.zsh;
-    extraGroups = [ 
-      "networkmanager" 
-      "wheel"
-      "docker"
-    ];
-    packages = with pkgs; [];
-  };
-  environment.shells = with pkgs; [ zsh ];
-   
-  # Window manager
-  programs.hyprland = {
-    enable = true;
-    xwayland.hidpi = true;
-    xwayland.enable = true;
-  };
-  environment.sessionVariables = {
-    # Hint electron apps to use wayland
-    NIXOS_OZONE_WL = "1";
-  };
-
-  # Helps enable screen sharing
-  services.dbus.enable = true;
-  xdg.portal = {
-    enable = true;
-    wlr.enable = true;
-    extraPortals = [
-      pkgs.xdg-desktop-portal-gtk
-    ];
-  };
-
-  # Enable sound with pipewire
-  # sound.enable = true;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-  };
-
-  # Enable Docker
-  virtualisation.docker.enable = true;
-
-  # Automatic Garbage Collection
-  nix.gc = {
-    automatic = true;
-    dates = "weekly";
-    options = "--delete-older-than 7d";
-  };
-  # List services that you want to enable:
-  # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
-
-  # Enable touchpad
-  services.xserver.libinput.enable = true;
-  
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
@@ -130,12 +20,4 @@
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "23.05"; # Did you read the comment?
-  
-  # nixpkgs.overlays = [
-  #   (self: super: {
-  #     waybar = super.waybar.overrideAttrs (oldAttrs: {
-  #       mesonFlags = oldAttrs.mesonFlags ++ [ "-Dexperimental=true" ];
-  #       });
-  #     })
-  #  ];  
 }
---
+++ system/base.nix
@@ -0,0 +1,118 @@
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ 
+      <home-manager/nixos>
+    ];
+
+  # ENV variables
+  environment.variables = {
+    EDITOR = "nvim";
+  };
+ 
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.extraModulePackages = [ config.boot.kernelPackages.v4l2loopback.out ];
+  boot.kernelModules = [
+    "v4l2loopback"
+  ];
+  boot.extraModprobeConfig = ''
+    options v4l2loopback exclusive_caps=1 card_label="Virtual Camera"
+  '';
+
+  # Set your time zone.
+  time.timeZone = "America/Chicago";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
+  };
+
+  # Configure keymap in X11
+  services.xserver = {
+    layout = "us";
+    xkbVariant = "";
+    enable = true;
+    displayManager.sddm.enable = true;
+  };
+ 
+  # Home manager
+  home-manager = {
+    useGlobalPkgs = true;
+    useUserPackages = true;
+    users.josiah = import /etc/nixos/home.nix;
+  };
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.josiah = {
+    isNormalUser = true;
+    description = "Josiah";
+    shell = pkgs.zsh;
+    extraGroups = [ 
+      "networkmanager" 
+      "wheel"
+      "docker"
+    ];
+    packages = with pkgs; [];
+  };
+  environment.shells = with pkgs; [ zsh ];
+   
+  # Window manager
+  programs.hyprland = {
+    enable = true;
+    xwayland.hidpi = true;
+    xwayland.enable = true;
+  };
+  environment.sessionVariables = {
+    # Hint electron apps to use wayland
+    NIXOS_OZONE_WL = "1";
+  };
+
+  # Helps enable screen sharing
+  services.dbus.enable = true;
+  xdg.portal = {
+    enable = true;
+    wlr.enable = true;
+    extraPortals = [
+      pkgs.xdg-desktop-portal-gtk
+    ];
+  };
+
+  # Enable sound with pipewire
+  # sound.enable = true;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
+
+  # Enable Docker
+  virtualisation.docker.enable = true;
+
+  # Automatic Garbage Collection
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 7d";
+  };
+  # List services that you want to enable:
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+
+  # Enable touchpad
+  services.xserver.libinput.enable = true;
+}
